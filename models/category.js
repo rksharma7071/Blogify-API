a4e2d75fab6ae4a44c13ea32fc4c764caf526aa7
Retesh Kumar Sharma <rksharma7071@gmail.com>
2025-05-07 19:18:29 +0530

    version 0.0.3

--- models/category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  description: { type: String },
-}, { timestamps: true });
-
-const Category = mongoose.model('Category', categorySchema);
-module.exports = Category;
